8ed35182e9640b7029bb251f12b9ff13eb6f5044
dylanmhoke <dylan.m.hoke@outlook.com>
2024-01-08 14:51:01 -0800

    Update

--- Graphs/DepthFirstSearchAdj.py
+++ Graphs/DepthFirstSearchAdj.py
@@ -1,35 +1,20 @@
 from collections import deque
 
 class ListNode:
-
     def __init__(self, x):
         self.value = x
         self.left = None
         self.right = None
 
 def solution(adj_list:list[ListNode], start:ListNode) -> list[ListNode]:
-
-    visited = {} # set()
-
-    stack = deque()
-
-    stack.append(start.value)
-
-    # while len(stack) != 0:
-    while stack:
-
-        toVisit = stack.pop()
-
+    visited = set()
+    Dq = deque()
+    Dq.append(start)
+    while Dq:
+        toVisit = Dq.pop()
         if toVisit in visited:
-
             continue
-
-        # visited.add(toVisit)
         visited.add(toVisit)
-
-        for neighbor in adj_list[toVisit]:
-
-            stack.append(neighbor.value)
-
+        for neighbor in adj_list[toVisit.value]:
+            Dq.append(neighbor)
     return list(visited)
-    
--- Graphs/DepthFirstSearchCycles.py
+++ Graphs/DepthFirstSearchCycles.py
@@ -1,28 +1,15 @@
 def solution(connections:list[list[int]]) -> bool:
-
     visited = set()
-
     def dfs(start:int) -> bool:
-        #depth first search
         if start in visited:
-            #if you already have it
             return True
-        
         visited.add(start)
-
         for neighbor in connections[start]:
-
             if dfs(neighbor):
-                
-                return True
-            
+                return True 
         visited.remove(start)
-
         return False
-
     for node in range(len(connections)):
-
         if dfs(node):
             return True
-        
     return False
