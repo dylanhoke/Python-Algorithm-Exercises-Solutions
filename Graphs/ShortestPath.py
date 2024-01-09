bffc6b8fbcc6f55ff7f3d6037e872f08e1a2d0a3
dylanmhoke <dylan.m.hoke@outlook.com>
2024-01-08 16:40:07 -0800

    Update

--- Graphs/ShortestPath.py
+++ Graphs/ShortestPath.py
@@ -1,47 +1,20 @@
-# def solution(adj_list, start, end):
-#     n = len(adj_list)
-#     visited = [False] * n
-#     queue = [[start]]
-
-#     while queue:
-#         path = queue.pop(0)
-#         current_city = path[-1]
-
-#         if current_city == end:
-#             return path
-
-#         if not visited[current_city]:
-#             visited[current_city] = True
-#             neighbors = adj_list[current_city]
-#             for neighbor in neighbors:
-#                 new_path = path + [neighbor]
-#                 queue.append(new_path)
-
-#     return []
-
 from collections import deque
 
 def solution(adj_list, start, end):
     n = len(adj_list)
-    
     if start < 0 or start >= n or end < 0 or end >= n:
         return []
-
     visited = [False] * n
     queue = deque([[start]])
-
     while queue:
         path = queue.popleft()  # Use popleft for efficient queue operations
         current_city = path[-1]
-
         if current_city == end:
             return path
-
         if not visited[current_city]:
             visited[current_city] = True
             neighbors = adj_list[current_city]
             for neighbor in neighbors:
                 new_path = path + [neighbor]
                 queue.append(new_path)
-
     return []
--- Graphs/WaitforitGraph.py
+++ Graphs/WaitforitGraph.py
@@ -1,36 +1,18 @@
 def solution(connections):
-    
-    
     visited = set()
-    
     def dfs(start, parent):
-        
         if start in visited:
-            
             return True
-            
         visited.add(start)
-        
         for neighbor in connections[start]:
-
             if neighbor != parent:
-
                 cyclic = dfs(neighbor, start)
-                
                 if cyclic:
-                    
                     return True
-                
         visited.remove(start)
-        
         return False
-        
     for node in range(len(connections)):
-        
         detectCycle = dfs(node, None)
-        
         if detectCycle:
-            
-            return True
-            
+            return True 
     return False
